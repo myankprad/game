c1354b85cf96727cfb0730fea4c5746980f176e7
[mayank pradhan] <mayankpradhan115@gmail.com>
2023-04-28 15:02:43 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const arr = ["IronMan", "Thanos", "CaptainSteve", "Thor"]
+   
+  const [arrCopy, setArrCopy] = useState(arr)
+
+  const deleteHero = (itemIndex)=>{
+    // let newArr = arrCopy;
+    // let filteredArr = newArr.filter((el, i)=>{
+    //      return i !== itemIndex
+    // })
+
+    // setArrCopy(filteredArr)
+
+   let newArr = arrCopy
+   let filteredArr = newArr.filter((el, i)=>{
+      return i !== itemIndex
+   })
+    setArrCopy(filteredArr)
+
+  }
+
+
+
   return (
-    <div className="App">
-      <h1>Choose rock paper or scissor</h1>
-    </div>
+    <div className="app">
+    <ul>
+     {
+      arrCopy.map((el, i)=>{
+       return ( 
+        <li>
+         <button>X</button>
+
+         {el}
+        <button onClick={()=> deleteHero(i) }>Delete</button>
+        
+         </li>
+        )
+      })
+     }
+    </ul>
+   </div>
   );
 }
 
